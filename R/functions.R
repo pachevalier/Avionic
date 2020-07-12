3587f71ebfc11b03462f21d791797868e76cf70f
Paul-Antoine Chevalier <pachevalier@gmail.com>
2020-07-12 15:55:23 +0200

    new function coef_technique()

--- R/functions.R
+++ R/functions.R
@@ -28,3 +28,36 @@ identite <- function(A) {
   I <- diag(nrow = rang)
   return (I-A) 
  }
+
+
+#' coef_techniques
+#'
+#' @param A a matrix
+#' @param B a matrix
+#'
+#' @return a matrix
+#' @export
+#'
+#' @examples
+#' 
+coef_techniques <- function (A,B) {
+  tei <- A
+  prod <- B
+  ctec <- matrix(nrow = nrow(tei), ncol = ncol(tei))
+  if (ncol(tei) == length(prod)) {
+    for (i in 1:nrow(tei)) {
+      for (j in 1:ncol(tei)) {
+        if (prod[j]==0) {
+          ctec[i,j] <- 0
+        }else{
+          ctec[i,j] <- tei[i,j]/prod[j]
+        }
+      }
+    }
+    return (ctec)
+  }
+  else {
+    message("Erreur : la taille du vecteur de production 
+            et du TEI ne sont pas cohérentes")
+  }
+}
