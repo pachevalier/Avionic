0f800705f319f679c794dfb3dd3be24d4c60d672
Paul-Antoine Chevalier <pachevalier@gmail.com>
2020-07-12 15:50:11 +0200

    add identite function

--- R/functions.R
+++ R/functions.R
@@ -11,3 +11,20 @@ matrice <- function(fichier) {
   A <- as.matrix(read.table(fichier, sep=";"))
   return (A)
 }
+
+#' Identite
+#'
+#' @param A a matrix 
+#'
+#' @return a matrix
+#' @export
+#'
+#' @examples
+#' A <- matrix(data = 1:16, nrow = 4)
+#' identite(A)
+#' 
+identite <- function(A) {
+  rang <- nrow(A)
+  I <- diag(nrow = rang)
+  return (I-A) 
+ }
